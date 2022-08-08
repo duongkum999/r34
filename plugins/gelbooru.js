51f898d2a4bebaa4814faecf85893f392b07ac7a
duongkum999 <85815383+duongkum999@users.noreply.github.com>
2022-08-08 18:17:46 +0700

    Create gelbooru.js

---
+++ plugins/gelbooru.js
@@ -0,0 +1,20 @@
+const axios = require("axios");
+module.exports = {
+  name: "gelbooru",
+  run: async (req, res) => {
+    var { search } = req.query;
+    if(!search) return res.json({error : "Dcm vui lòng nhập từ khóa tìm kiếm!"});
+    try {
+      var url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=" + encodeURI(search).replace("%20", "_");
+      const get = await axios.get(url);
+      var post = get.data.post;
+      var data = [];
+      for(let a of post) {
+        data.push(a.file_url.replace(/\/\//g, '//').replace(/\//g, '/'));
+      }
+      return res.json(data);
+    } catch (error) { 
+      return res.json({error : `${error}`});
+    }
+  }
+};
