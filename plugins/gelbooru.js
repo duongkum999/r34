759c013fadb7f4e1e5bafc0949d9a8df455b7df0
duongkum999 <85815383+duongkum999@users.noreply.github.com>
2022-08-09 09:34:15 +0700

    Update gelbooru.js

--- plugins/gelbooru.js
+++ plugins/gelbooru.js
@@ -7,14 +7,14 @@ module.exports = {
     try {
       var url = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tags=" + encodeURI(search).replace("%20", "_");
       const get = await axios.get(url);
-      var count = get.data.@attributes.count;
+      var count = get.data.attributes.count;
       if (count == 0) return res.json({ count: 0, data: []});
       var data = [];
       var pageid = Math.floor(count/100);
       pageid = pageid > 10 ? 10 : pageid;
       for (let i = 1; i <= pageid; i++) {
         var getall = await axios.get(url + "&pid=" + i);
-        var post = get.data.post;
+        var post = getall.data.post;
         var format = ["jpeg", "jpg", "png"];
         for(let a of post) {
           for (let b of format) {
