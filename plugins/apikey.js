9469907b3ddf024f60a8772af2f7589b96dbba1d
duongkum999 <85815383+duongkum999@users.noreply.github.com>
2022-08-10 23:58:51 +0700

    Update apikey.js

--- plugins/apikey.js
+++ plugins/apikey.js
@@ -31,3 +31,66 @@ module.exports = {
     } catch (error) { return res.json({error : `${error}`}); }
   }
 };
+var apikey = [
+
+  "8adb5ee609123e2289db00d46dbfb27a5a6977429d335eda308b899fe5acba2e",
+
+  "382d29d670374903684f737760014bd2ce8f48bd8e751655ec970e2e0df8885d"
+
+];
+
+module.exports = {
+
+  name: "yandex",
+
+  run: async (req, res) => {
+
+    const axios = require("axios");
+
+    var { search } = req.query;
+
+    if(!search) return res.json({error : "Dcm vui lòng nhập từ khóa tìm kiếm!"});
+
+    try {
+
+      var key = "&api_key=" + apikey[Math.floor(Math.random()*apikey.length)];
+
+      var url = "https://serpapi.com/search.json?engine=yandex_images&text=" + search + key;
+
+      const get = (await axios.get(url)).data;
+
+      if ('error' in get) return res.json({ search: search, count: 0, data: []});
+
+      const endpoint = get.search_metadata.json_endpoint + key;
+
+      var nextpage = get.serpapi_pagination.next + key;
+
+      for( var image of get.images_results) { data.push(image.original); }
+
+      var check = 1;
+
+      var data = [];
+
+      var urlpage = "";
+
+      while (urlpage !== endpoint && check <= 10) {
+
+        var getnextpage = (await axios.get(nextpage)).data;
+
+        for(var image of getnextpage.images_results) { data.push(image.original); }
+
+        check++;
+
+        urlpage = nextpage;
+
+        nextpage = getnextpage.serpapi_pagination.next + key;
+
+	  }
+
+      return res.json({ search: search, count: data.length, data: data});
+
+    } catch (error) { return res.json({error : `${error}`}); }
+
+  }
+
+};
